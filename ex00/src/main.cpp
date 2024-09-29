2c0ba8008ad545425edd6243200cf215e930fa8a
Iurii Ovsiannikov <jgoldste@z1t6c2.42bangkok.com>
2024-09-29 18:33:15 +0700

    ex00 main.cpp 10000 random test cases added

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,13 +1,27 @@
 #include "BitwiseAdd.hpp"
+#include <random>
 
 int main() {
-    try {
-        std::cout << "bitwiseAdd(5, 2) = " << BitwiseAdd::bitwiseAdd(5, 2) << std::endl;
-        std::cout << "bitwiseAdd(23, 4) = " << BitwiseAdd::bitwiseAdd(23, 4) << std::endl;
-        std::cout << "bitwiseAdd(-5, 2) = " << BitwiseAdd::bitwiseAdd(-5, 2) << std::endl; // This will throw an exception
-    } catch (const BitwiseAdd::InputError& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    std::random_device rd;                                // Seed for random number engine
+    std::mt19937 gen(rd());                               // Mersenne Twister engine seeded with random_device
+    std::uniform_int_distribution<> dist(-100, 10000);    // Uniform distribution between 0 and 10000
 
+    for (int i = 0; i < 10000; i++) {
+        // Generate two random integers 'a' and 'b'
+        int a = dist(gen);
+        int b = dist(gen);
+        int result = 0; 
+        try {
+            result = BitwiseAdd::bitwiseAdd(a, b);
+            if (result != a + b) {
+                std::cerr << "Error[" << i << "]: " << a << " * " << b << " != " << result << std::endl;
+                return 1;
+            }
+            std::cout << a << " * " << b << " = " << result << std::endl;
+        } catch (const BitwiseAdd::InputError& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+    std::cout << "bitwiseAdd is working correctly" << std::endl;
     return 0;
 }
