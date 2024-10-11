330f016ea5437acf84df3f8f5b80d90b3df9cf76
Iurii Ovsiannikov <jgoldste@z1t5c2.42bangkok.com>
2024-10-11 19:20:54 +0700

    ex03 started

---
+++ ex03/include/booleanEvaluation.hpp
@@ -0,0 +1,13 @@
+#ifndef BOOLEANEVALUATION_HPP
+#define BOOBOOLEANEVALUATION_HPP
+
+class booleanEvaluation {
+    private:
+        booleanEvaluation() = delete;
+        ~booleanEvaluation() = delete;
+
+    public:
+        
+};
+
+#endif
